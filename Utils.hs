c68ef0ac9ac7ca6e5dfff5d067137061e994864d
Szkaradd <szkaradekmikolaj@gmail.com>
2023-05-14 14:19:35 +0200

    changed qualified

--- Utils.hs
+++ Utils.hs
@@ -4,7 +4,7 @@ import Control.Monad.Except
 import Control.Monad.Reader
 import Control.Monad.State
 import Data.Map (Map)
-import Data.Map qualified as Map
+import qualified Data.Map as Map
 import Szkarson.Abs
 import Types
 
