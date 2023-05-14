383f52d2828365c0298c5ad1ad8b714459b01df6
Szkaradd <szkaradekmikolaj@gmail.com>
2023-05-14 18:48:56 +0200

    updated scripts

--- run_bad.sh
+++ run_bad.sh
@@ -4,10 +4,12 @@ for file in bad/*.szk
 do
     echo "Running $file"
     ./interpreter $file
+    echo "-------------------"
 done
 
 for file in bad/parse_errors/*.szk
 do
     echo "Running $file"
     ./interpreter $file
+    echo "-------------------"
 done
--- run_good.sh
+++ run_good.sh
@@ -4,4 +4,5 @@ for file in good/*.szk
 do
     echo "Running $file"
     ./interpreter $file
+    echo "-------------------"
 done
